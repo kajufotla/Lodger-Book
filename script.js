ea13931534e37d8152c334a258df926d7200da00
kajufotla <kajufotla@gmail.com>
2026-06-28 04:27:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+class AppUI { /* آپ کا سارا AppUI کوڈ */ }
+
+class PDFEngine { /* آپ کا سارا PDFEngine کوڈ */ }
+
+// ٹولز کو رینڈر کرنے والا لوپ
+const grid = document.getElementById('tools-grid');
+Object.keys(ToolsConfig).forEach(key => { /* آپ کا سارا لوپ والا کوڈ */ });
+
+// ورک اسپیس ایکٹیویٹ کرنے والا فنکشن
+window.activateWorkspace = function(id) { /* آپ کا کوڈ */ }
